3e35fb92271b226324b9f5f9281769c1af53cb18
rzdigitalrankings <rozyrout5844@gmail.com>
2026-02-14 21:34:52 +0530

    Add files via upload

---
+++ influencersinindia/influencersinindia/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{padding:40,fontFamily:'sans-serif'}}>
+      <h1>InfluencersInIndia</h1>
+      <p>India’s Trending Celebrity Endorsement Platform</p>
+      <p>Platform is production ready.</p>
+    </main>
+  );
+}
